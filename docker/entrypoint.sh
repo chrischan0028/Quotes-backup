c57530376c1be4ccc503f7bd79ee5e5b8b26e340
chrischan0028 <chris.lightmoon0917@gmail.com>
2026-02-05 10:19:26 -0500

    docker compression

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 set -e
 
-echo "Installing fresh Laravel..."
+echo "Creating fresh Laravel project..."
+composer create-project laravel/laravel quotes-app --no-interaction
 
-composer create-project laravel/laravel laravel-app --no-interaction
+cd quotes-app
 
-cd laravel-app
+echo "Allowing dev stability for local package..."
+composer config minimum-stability dev
+composer config prefer-stable true
 
 echo "Copying Quotes package..."
 mkdir -p packages/MyVendor/Quotes
 cp -R /package/* packages/MyVendor/Quotes/
+echo "Copying Quote blades page..."
+cp -R /package/resources/views/* resources/views/
 
-echo "Registering Composer path repository..."
-
+echo "Registering package path repository..."
 composer config repositories.quotes path packages/MyVendor/Quotes
 
+echo "Installing Quotes package..."
 composer require my-vendor/quotes:"*" --no-interaction
+composer update
 
-echo "Installing Node dependencies for package..."
+echo "Building package frontend assets..."
 cd packages/MyVendor/Quotes
 npm install
 npm run build
 cd ../../../
 
-echo "Publishing package assets..."
+echo "Publishing package config & assets..."
 php artisan vendor:publish --provider="MyVendor\Quotes\QuotesServiceProvider" --tag=config --force
 php artisan vendor:publish --provider="MyVendor\Quotes\QuotesServiceProvider" --tag=assets --force
 
-echo "Migrating database..."
+echo "Running migrations..."
 php artisan migrate --force
 
 echo "Clearing caches..."
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-php artisan cache:clear
+php artisan optimize:clear
 
 echo "Starting Laravel server..."
 php artisan serve --host=0.0.0.0 --port=8000
---
+++ resources/views/quotes-ui.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quotes UI</title>
+</head>
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('vendor/quotes/dist/quotes-ui.js') }}"></script>
+</body>
+</html>
